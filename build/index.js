ea212b7defd71df3d84c1997391b68c03378b002
Alien Padilla Rodriguez <alienpr84@gmail.com>
2023-05-14 16:24:51 -0300

    initial setup

---
+++ build/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const sum = (numbers) => numbers.reduce((acumulator, number) => acumulator += number);
+console.log(sum([3, 4, 1, 8]));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const sum = (numbers: number[]): number => numbers.reduce(
+  (acumulator, number) => acumulator+=number
+);
+
+console.log(sum([3, 4, 1, 8]))
